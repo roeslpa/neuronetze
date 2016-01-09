4b2e66fd53ec5822f306c4b42ba5bb3fd4215644
roeslpa <git@roeslpa.de>
2016-01-09 17:18:20 +0100

    initial commit

---
+++ Uebung4/MLP.h
@@ -0,0 +1,16 @@
+#ifndef KNN4_MLP_H
+#define KNN4_MLP_H
+
+#include "matrix.h"
+
+using namespace std;
+
+class MLP{
+
+public:
+
+private:
+
+};
+
+#endif // KNN4_MLP_H
---
+++ Uebung4/main.cpp
@@ -0,0 +1,7 @@
+#include "matrix.h"
+#include "MLP.h"
+
+int main(int argc, char** argv)
+{
+
+}
