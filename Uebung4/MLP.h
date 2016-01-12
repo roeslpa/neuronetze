2293c6425a62b2bce0dc68e333fd44fbda537af3
pnoltof <daniel.teuchert@rub.de>
2016-01-13 00:39:07 +0100

    Aufgabe weiter gemacht

--- Uebung4/MLP.h
+++ Uebung4/MLP.h
@@ -10,8 +10,9 @@ class MLP{
 public:
 
 	MLP(knn::matrix wE, unsigned ME);
-	double get(double xE);
+	double getY(double xE);
 	void calcError(double xE, double tE); // Für Aufgabe 4.2 a)
+	void gradientDescent(double x, double t, double lernrate); //Für Aufgabe 4.2 a)
 
 private:
 
@@ -22,6 +23,7 @@ private:
 	
 	knn::matrix w;
 	knn::matrix z;
+	double x;
 
 	double fact(double a);
 };
@@ -30,7 +32,8 @@ MLP::MLP(knn::matrix wE, unsigned ME) {
 	w = wE;
 	M = ME;
 	//x = xE; in die get methode verschoben
-	z = knn::matrix(1, M);
+	
+	z = knn::matrix(1, M+1);
 	z(1,1)=1; //z0=1
 }
 
@@ -39,25 +42,50 @@ double MLP::fact(double a) {
 }
 
 //Aufgabe e)
-double MLP::get(double xE) {
+double MLP::getY(double xE) {
 	x = xE; //muss das nicht hier hin?
 	//z0 = 1
-	y = w(3,1); //durfte ich das ändern?
+	y = w(3,1); 
 	
 	for(unsigned m=2; m<=M+1; m++) {
-		z(1,m) = fact(w(1,m)+w(2,m)*x); //schauen welches w_01 und w_11 ist |||| Durfte ich das ändern?
+		z(1,m) = fact(w(1,m)+w(2,m)*x); //schauen welches w_01 und w_11 ist 
 		y += w(3,m)*z(1,m);
 	}
 	
 	return y;
 }
 //Aufgabe 4.2 a)
-void calcError(double xE, double tE)
+void MLP::calcError(double xE, double tE)
 {
-	deltaExit = get(xE)-tE;
-	deltaHid = knn::matrix(1,M);
-	for(unsigned m=1; m<=M; m++) {
+	deltaExit = getY(xE)-tE;
+	deltaHid = knn::matrix(1,M+1); // deltaHid(1,1) wird nicht benötigt. Dies ist nur um den Index and die Indizierung der Gewichte an zupassen.
+	for(unsigned m=2; m<=M+1; m++) {
 	deltaHid(1,m)= z(1,m)*(1-z(1,m))*deltaExit*w(3,m);
+	}
+}
+void MLP::gradientDescent(double x, double t, double lernrate)
+{
+	knn::matrix deltaW = knn::matrix(3,M+1);
+	calcError(x,t);
+	//Berechne deltaW0^(2)
+	deltaW(3,1)= -(lernrate*deltaExit);
+	//Berechne deltaWm^(2)
+	for (unsigned m=2;m<=M+1;m++)
+	{
+		deltaW(3,m)= -(lernrate*deltaExit*z(1,m));
+	}
+	//Berechne deltaW0m^(1) und deltaW1m^(1)
+	for (unsigned m=2;m<=M+1;m++)
+	{
+		deltaW(1,m)= -(lernrate*deltaHid(1,m));
+		deltaW(2,m)= -(lernrate*deltaHid(1,m)*x);
+	}
+	//Neue Gewichte berechnen:
+	for (unsigned m=1;m<=M+1;m++)
+	{
+		w(1,m)+= deltaW(1,m);
+		w(2,m)+= deltaW(2,m);
+		w(3,m)+= deltaW(3,m);
 	}
 }
 #endif // KNN4_MLP_H
--- Uebung4/main.cpp
+++ Uebung4/main.cpp
@@ -8,19 +8,49 @@ int main(int argc, char** argv)
 	unsigned M = 20;
 	double wMin = -4;
 	double wMax = 4;
-
-	knn::matrix w;
+	unsigned P = 50;
+	double lernrate = 0.01;
+	
+	knn::matrix w,tP, xP, epsilonP;
+	double epsilonMin = -0.15;
+	double epsilonMax = 0.15;
+	
 	double x;
 	ofstream output41e;
 
-	w = knn::matrix(3, M+1); //darf ich das ändern?
+	//Aufgabe 4.1 f)
+	w = knn::matrix(3, M+1); 
 	w.fillRandom(wMin, wMax);
 	output41e.open("out4.1.e.txt"); //ist das nicht aufgabe f?
 
+	MLP mlp = MLP(w, M);
 	for(unsigned i=0; i<1000; i++) {
 		x = knn::randomFromInterval(xMin, xMax);
-		MLP mlp = MLP(w, M);
-		output41e << x << " " << mlp.get(x) << "\n";
+		//MLP mlp = MLP(w, M); For die Schleife gezogen (muss doch nicht jedesmal neu initialisiert werden oder?)
+		output41e << x << " " << mlp.getY(x) << "\n";
 	}
 	output41e.close();
+	
+	//Aufgabe 4.2 a)
+	//Trainingsbeispiele erzeugen:
+	tP= knn::matrix(1, P);
+	xP= knn::matrix(1,P);
+	epsilonP = knn::matrix(1,P);
+	epsilonP.fillRandom(epsilonMin, epsilonMax);
+	for (unsigned p=1; p<=P; p++)
+	{
+		xP(1,p)= (15*p)/(P-1)-7.5;
+		tP(1,p)= exp(-(pow(xP(1,p),2)/10))+epsilonP(1,p);
+	}
+	//Gewichte zufällig füllen:
+	wMin = -5;
+	wMax = 5;
+	w.fillRandom(wMin, wMax);
+	//Gradientenabstieg für eine Epoche:
+	for (unsigned p=1; p<=P;p++)
+	{
+		mlp.gradientDescent(xP(1,p), tP(1,p), lernrate);
+	}
+	
+	//Aufgabe 4.2 b)
 }
