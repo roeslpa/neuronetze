82510ab6575c22f342179500e0d7108cb04444ac
pnoltof <daniel.teuchert@rub.de>
2016-01-27 19:17:04 +0100

    Fehler in main.cpp Zeile 41

--- Uebung5/main.cpp
+++ Uebung5/main.cpp
@@ -2,10 +2,11 @@
 #include "MIMLP.h"
 
 //Funktionen:
-knn::matrix calcXVector(unsigned int x);
+knn::matrix* calcXVector(unsigned int x);
 
 // Globale Variblen:
 int D;
+
 
 
 int main(int argc, char** argv)
@@ -14,26 +15,38 @@ int main(int argc, char** argv)
 	int D = 9;
 	int M = 2;
 	unsigned P = (int)pow(2, D-1);
-	knn::matrix xP = knn::matrix(1,P);
-	knn::matrix tP = knn::matrix(1,P);
-	unsigned tLatest, xLatest; // Hilfsvariablen um zu wissen wie viele Werte bereits in xP bzw. tP gespeichert sind.		
+	knn::matrix xP = knn::matrix(1,P); // Eingabewerte der Trainingsbeispiele
+	knn::matrix tP = knn::matrix(1,P); // Erwartete Ausgabewerte der Trainingsbeispiele
+	knn::matrix xT = knn::matrix(1,P); // Eingabewerte der Testbeispiele
+	unsigned xTLatest, xPLatest; // Hilfsvariablen um zu wissen wie viele Werte bereits in xP bzw. xT gespeichert sind.		
 	
 	// 5.2 b)
 	// Die Test- und Trainingsbeispiele werden als ganze Zahlen gespeichert (dies spart Speicherplatz). Aus diesen kann bei Bedarf mithilfe der Funktion calcXVector der entsprechende Eingabevektor berechnet werden.
-	tLatest = 0;
-	xLatest = 0;
+	xPLatest = 0;
+	xTLatest = 0;
 	// Mögliche eingabe Werte als ganze Zahl dargestellt gehen von 0 bis 2^D-1
 	for (unsigned int i = 0; i < 2*P;i++)
 	{
-		if ((tLatest>=P)||(((rand()%2)==0) && xLatest<P)) // Falls xP schon voll ist, oder der Zufallswert 1 modulo 2 entspricht und wird die aktuelle Zahl in tP gespeichert. Andernfalls in xP.
+		if ((xTLatest>=P)||(((rand()%2)==0) && xPLatest<P)) // Falls xT schon voll ist, oder der Zufallswert 1 modulo 2 entspricht und wird die aktuelle Zahl in xP gespeichert. Andernfalls in xT.
 		{
-			xLatest++;
-			xP(1,xLatest)=i;
+			//Trainingsbeispiele
+			xPLatest++;
+			xP(1,xPLatest)=i;
+			//Berechnung fon tP=f(xP)
+			unsigned int sum = 0;
+			knn::matrix xd;
+			xd = *calcXVector(i);
+			for(unsigned d=2;d<=D+1;d++ )
+			{
+				sum += xd(1,d); // wenn hier sum += xd(1,1) steht geht es. SOnst nicht.
+			}
+			tP(1, xPLatest)= pow(-1, sum+1);
 		}
 		else
 		{
-			tLatest++;
-			tP(1,tLatest)=i;
+			//Testbeispiele
+			xTLatest++;
+			xT(1,xTLatest)=i;
 		}
 	}
 	
@@ -43,13 +56,14 @@ int main(int argc, char** argv)
 
 
 //Bit-Vektor aus Eingabewert berechnen (etwas anders als die Formel vom Hilfsblatt, aber dennoch korrekt)
-knn::matrix calcXVector(unsigned int x)
+knn::matrix* calcXVector(unsigned int x)
 {
-	knn::matrix xd = knn::matrix(1, D+1);
+	knn::matrix *xd = new knn::matrix(1, D+1);
+	/*
 	xd(1,1)=1; //x0=1
 	for(unsigned d=2; d<=D+1; d++)
 	{
 		xd(1,d)= ((x&(int)pow(2, d-1))!=0?1:0);
-	}
+	}*/
 	return xd;
 }
