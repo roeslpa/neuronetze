d8133d01cf3b2f225d8f3240312e104584629169
roeslpa <git@roeslpa.de>
2016-01-27 18:34:36 +0100

    c hinzugefuegt

--- Uebung5/main.cpp
+++ Uebung5/main.cpp
@@ -16,7 +16,13 @@ int main(int argc, char** argv)
 	unsigned P = (int)pow(2, D-1);
 	knn::matrix xP = knn::matrix(1,P);
 	knn::matrix tP = knn::matrix(1,P);
-	unsigned tLatest, xLatest; // Hilfsvariablen um zu wissen wie viele Werte bereits in xP bzw. tP gespeichert sind.		
+	unsigned tLatest, xLatest; // Hilfsvariablen um zu wissen wie viele Werte bereits in xP bzw. tP gespeichert sind.
+	double lernrate = 0.01;
+	double wMin = -2;
+	double wMax = 2;
+	double trainingsfehler;
+	double testfehler;
+	
 	
 	// 5.2 b)
 	// Die Test- und Trainingsbeispiele werden als ganze Zahlen gespeichert (dies spart Speicherplatz). Aus diesen kann bei Bedarf mithilfe der Funktion calcXVector der entsprechende Eingabevektor berechnet werden.
@@ -37,10 +43,26 @@ int main(int argc, char** argv)
 		}
 	}
 	
-	
-	
-}
 
+	MIMLP mimlp22 = trainingslauf(2, 2);
+	MIMLP mimlp25 = trainingslauf(2, 5);
+	MIMLP mimlp28 = trainingslauf(2, 8);
+	MIMLP mimlp210 = trainingslauf(2, 10);
+
+	MIMLP mimlp102 = trainingslauf(10, 2);
+	MIMLP mimlp105 = trainingslauf(10, 5);
+	MIMLP mimlp108 = trainingslauf(10, 8);
+	MIMLP mimlp1010 = trainingslauf(10, 10);
+
+	/*Beispielausgabe:
+	2 1 0.017133
+	10 1 0.00936311
+	60 7 0.00701061
+	Die Laufzeit verlängert sich, wie erwartet mit steigender Komplexität des Netzwerkes (Pardon für die Ungenauigkeit).
+	Die Genauigkeit steigt von M=2 zu M=10 rapide, von M=10 zu M=60 aber nur noch sehr wenig. Dies steht in keinem Verhältnis zur
+	deutlich längeren Laufzeit.
+	*/	
+}
 
 //Bit-Vektor aus Eingabewert berechnen (etwas anders als die Formel vom Hilfsblatt, aber dennoch korrekt)
 knn::matrix calcXVector(unsigned int x)
@@ -52,4 +74,42 @@ knn::matrix calcXVector(unsigned int x)
 		xd(1,d)= ((x&(int)pow(2, d-1))!=0?1:0);
 	}
 	return xd;
+}
+
+MIMLP trainingslauf(unsigned M, unsigned D)
+{
+	knn::matrix w1 = knn::matrix(D+1,M+1);
+	knn::matrix w2 = knn::matrix(1, M+1);
+	w1.fillRandom(wMin, wMax);
+	w2.fillRandom(wMin, wMax);
+	MIMLP mimlp = MIMLP(w1,w2,M,D);
+	trainingsfehler = 0;
+	testfehler = 0;
+	time_t zeit;
+	long anfang = time(&zeit);
+	for (unsigned i=0; i<10000;i++)
+	{
+		// Gradientenabstieg für eine Epoche
+		for (unsigned p=1; p<=P;p++)
+		{
+			mimlp.gradientDescent(calcXVector(xP(1,p)), tP(1,p), lernrate);
+		}
+
+		//Trainingsfehlerberechnung
+		for (unsigned p=1;p<=P;p++)
+		{
+			trainingsfehler+= pow(mimlp.getY(calcXVector(xP(1,p)))-tP(1,p),2);
+		}
+		trainingsfehler = trainingsfehler/P;
+
+		//Testfehlerberechnung
+		for (unsigned p=1;p<=P;p++)
+		{
+			testfehler+= pow(mimlp.getY(calcXVector(yP(1,p)))-sP(1,p),2);
+		}
+		testfehler = testfehler/P;
+	}
+	cout << M << " " << D << " " << time(&zeit)-anfang << " " << trainingsfehler << " " << testfehler << endl;
+	
+	return mimlp;
 }
