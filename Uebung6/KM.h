d8126c4927065cccc2972389c880417dc865bebd
roeslpa <git@roeslpa.de>
2016-02-04 19:00:15 +0100

    initial commit

---
+++ Uebung6/KM.h
@@ -0,0 +1,16 @@
+#ifndef KNN6_KM_H
+#define KNN6_KM_H
+
+#include "matrix.h"
+
+using namespace std;
+
+class KM{
+
+public:
+
+private:
+
+};
+
+#endif // KNN6_KM_H
---
+++ Uebung6/main.cpp
@@ -0,0 +1,7 @@
+#include "matrix.h"
+#include "KM.h"
+
+int main(int argc, char** argv)
+{
+
+}
