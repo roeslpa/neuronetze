ecab3dd8ddb9ba6835a5779724bf0a713e29cddc
pnoltof <daniel.teuchert@rub.de>
2016-02-05 19:47:59 +0100

    f) Überarbeitet

--- Uebung6/main.cpp
+++ Uebung6/main.cpp
@@ -1,7 +1,7 @@
 #include "matrix.h"
 #include "KM.h"
 // Paul Rösler und Daniel Teuchert
-// Es ist zu beobachten, dass die Zentren der Neuronen mit höhere Iterationenanzahl immer genauer die obere Hälfte eines Torus' formen. 
+// Es ist zu beobachten, dass die Zentren der Neuronen mit höherer Iterationenanzahl immer genauer die obere Hälfte eines Torus' formen. Ab 100000 Iterationen kann man jedoch beobachten, dass diese Form bereits wieder zerstört wird und bei 500000 Iterationen ist inzwischen nur noch weniger als die Hälfte eines Torus' erkennbar. Zwischen den Ausgaben der Unterschiedlichen Exponenten gibt es keine allzu großen Unterschiede.  Mit dem modifizierten Exponenten scheinen die Zentren der Neuronen jedoch etwas schneller die Form eines Torus' zu bilden.
 int main(int argc, char** argv) {
 	KM kohonenMap1 = KM(true);
 	knn::matrix x;
